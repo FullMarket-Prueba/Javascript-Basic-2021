2f3cb92dd6a593a913923f9975f14d6eed535e66
Edwin Rozo Gómez <erozog125@gmail.com>
2021-08-09 12:13:50 -0500

    Material de estudio de js básico

---
+++ 1.Variables/1.1Strings/strings.js
@@ -0,0 +1,62 @@
+// Todos aquellos datos que se consideren solamente para lectura serán tomados como Texto, es decir de tipo String.
+
+const product1 = "Tv LG de 20 Pulgadas";
+const product2 = '   Monitor de TV.    ';
+const product3 = String('TV');
+const product4 = new String('TV');
+const product5 = `Este es mi ${product1} `;
+const price = 500;
+
+// console.log(product1, product2, product3, product4, product5);
+
+//Hablemos de algunos métodos.
+// 1. length nos permite conocer la dimensión o cantidad de letras de la cadena de Texto.
+// console.log(product1.length);
+// Mostrar la posición en un elemento en la cadena
+// console.log(product5.indexOf('LG'));
+// Comprobar si un elemento existe en una cadena de texto
+// console.log(product5.includes('LG'));
+// console.log(product5.includes('Samsung'));
+
+// Eliminar espacios en blanco en inicio de una cadena
+// console.log(product2);
+// console.log(product2.trimStart());
+// console.log(product2.trimEnd());
+// console.log(product2.trimStart().trimEnd());
+// console.log(product2.trim());
+
+// Reemplazar elemento en una cadena de texto
+// console.log(product1);
+// console.log(product1.replace('Pulgadas','"'));
+
+// Cortar una cadena.
+// console.log(product1.slice(11,19));
+// console.log(product1.substring(11,19));
+
+// De izquierda a derecha
+// console.log(product1.slice(11,9));
+// console.log(product1.substring(11,9));
+
+// Obtener una posición exacta de la cadena de texto
+// console.log(product1.charAt(0));
+
+// Duplicar cadena de Texto
+// console.log(product1.repeat(3));
+
+// // dividir cadena de texto
+// console.log(product1.split(' '));
+
+// Convertir de mayúsculas a minúsculas y viceversa
+console.log(product1.toUpperCase());
+console.log(product1.toLowerCase());
+
+// convertir un número a String
+console.log(price);
+console.log(typeof price);
+console.log(typeof price.toString());
+console.log(price.toString());
+
+
+
+
+
---
+++ 1.Variables/variables2.js
@@ -0,0 +1,3 @@
+//Mismo nombre de variable con respecto al archivo variable.js
+var nameUser = 'Gustavo';
+console.log(nameUser);
